3961d7ac125b5baecc275333875e402789ecf0ca
André <silva.ala82@gmail.com>
2024-10-11 20:52:19 -0300

    feat: implements getters and setters of Category class entity

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -4,11 +4,14 @@ export type CategoryProperties = {
   description?: string,
   isActive?: boolean,
   createdAt?: Date
-
 }
 
 export class Category {
-  constructor(public readonly props: CategoryProperties) { }
+  constructor(public readonly props: CategoryProperties) {
+    this.description = this.props.description;
+    this.props.isActive = this.props.isActive ?? true;
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
 
   get name() {
     return this.props.name
@@ -18,8 +21,16 @@ export class Category {
     return this.props.description
   }
 
+  private set description(value: string) {
+    this.props.description = value ?? null;
+  }
+
   get isActive() {
     return this.props.isActive
+  }
+
+  private set isActive(value: boolean) {
+    this.props.isActive = value ?? true;
   }
 
   get createdAt() {
