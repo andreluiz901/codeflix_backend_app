30f12727f38a1ad3cbdaca1c8d8d0ae99ffc861a
André <silva.ala82@gmail.com>
2024-10-12 21:58:47 -0300

    refactor: put validate spyOn as a reusable const

--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -4,14 +4,14 @@ import UniqueEntityId from "./unique-entity-id.vo"
 
 describe('Unique entity id unit tests', () => {
 
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate")
+
   it('should throw error when uuid is invalid', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
     expect(() => new UniqueEntityId('fake id')).toThrow(new InvalidUuidError())
     expect(validateSpy).toHaveBeenCalled()
   })
 
   it('should accept a uuid passed in constructor', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
     const uuid = '42002e24-baea-41a7-9da2-6464319bc9c6'
     const vo = new UniqueEntityId(uuid)
     expect(vo.id).toBe(uuid)
@@ -19,7 +19,6 @@ describe('Unique entity id unit tests', () => {
   })
 
   it('should accept a uuid passed in constructor', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
     const vo = new UniqueEntityId()
     expect(uuidValidate(vo.id)).toBeTruthy()
     expect(validateSpy).toHaveBeenCalled()
