b60ea51f562147547a22ad416e3fa168aa2fa914
André <silva.ala82@gmail.com>
2024-10-15 02:18:21 +0000

    feat: implements new custom matcher to jest for tests

---
+++ src/@seedwork/domain/tests/validations.ts
@@ -0,0 +1,48 @@
+import { EntityValidationError } from "../errors/validation-error"
+import { ClassValidatorFields } from "../validators/class-validator-fields"
+import { FieldsErrors } from "../validators/validator-fileds-interface"
+import { expect } from 'expect'
+
+type Expected = { validator: ClassValidatorFields<any>, data: any } | (() => any)
+
+expect.extend({
+  containsErrorsMessages(expected: Expected, received: FieldsErrors) {
+
+    if (typeof expected === 'function') {
+      try {
+        expected()
+        return isValid()
+      } catch (e) {
+        const error = e as EntityValidationError
+        return assertContainsErrorsMessages(error.error, received)
+      }
+    } else {
+      const { validator, data } = expected
+      const validated = validator.validate(data)
+
+      if (validated) {
+        return isValid()
+      }
+
+      return assertContainsErrorsMessages(validator.errors, received)
+    }
+  }
+})
+
+function isValid() {
+  return { pass: true, message: () => '' }
+}
+
+function assertContainsErrorsMessages(expected: FieldsErrors, received: FieldsErrors) {
+  const isMatch = expect.objectContaining(received).asymmetricMatch(expected)
+
+  return isMatch
+    ? { pass: true, message: () => '' }
+    : {
+      pass: false,
+      message: () =>
+        `The validation errors do not contain ${JSON.stringify(
+          received
+        )}. Current: ${JSON.stringify(expected)}`,
+    }
+}
