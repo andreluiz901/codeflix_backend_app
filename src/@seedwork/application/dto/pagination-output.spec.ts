b0285dcc0a79cd49a06841f83d084a31757d47a6
André <silva.ala82@gmail.com>
2024-10-16 18:59:03 +0000

    test: ensures pagination output mapper

---
+++ src/@seedwork/application/dto/pagination-output.spec.ts
@@ -0,0 +1,23 @@
+import { SearchResult } from "../..//domain/repository/repository-contratcs"
+import { PaginationOutputMapper } from "./pagination-output"
+
+describe('PaginationOutputMapper Unit Test', () => {
+  it('should convert a search result in output', () => {
+    const result = new SearchResult({
+      items: ['fake'] as any,
+      total: 1,
+      currentPage: 1,
+      perPage: 1,
+      sort: 'name',
+      sortDir: 'desc',
+      filter: 'fake'
+    })
+    const output = PaginationOutputMapper.toOutput(result)
+    expect(output).toStrictEqual({
+      total: 1,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 1,
+    })
+  })
+})
