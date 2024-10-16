8a216755b796c035e5dc4021def9f94fb5aabdf0
André <silva.ala82@gmail.com>
2024-10-16 19:02:55 +0000

    test: ensures that category output mapper unit tests

---
+++ src/category/application/dto/category-output.spec.ts
@@ -0,0 +1,24 @@
+import { Category } from "../../domain/entities/category"
+import { CategoryOutputMapper } from "./category-output"
+
+describe('CategoryOutputMapper Unit Test', () => {
+  it('should convert a category in output', () => {
+    const createdAt = new Date()
+    const entity = new Category({
+      name: 'Movie',
+      description: 'some description',
+      isActive: true,
+      createdAt
+    })
+    const spyToJson = jest.spyOn(entity, 'toJSON')
+    const output = CategoryOutputMapper.toOutput(entity)
+    expect(spyToJson).toHaveBeenCalled()
+    expect(output).toStrictEqual({
+      id: entity.id,
+      name: 'Movie',
+      description: 'some description',
+      isActive: true,
+      createdAt
+    })
+  })
+})
