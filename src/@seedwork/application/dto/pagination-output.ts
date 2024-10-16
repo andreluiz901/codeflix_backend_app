249f7c1b5efac07ab0fe70d3a78a1b6f3ed1f685
André <silva.ala82@gmail.com>
2024-10-16 18:59:43 +0000

    feat: implements pagination output

---
+++ src/@seedwork/application/dto/pagination-output.ts
@@ -0,0 +1,20 @@
+import { SearchResult } from "@seedwork/domain/repository/repository-contratcs"
+
+export type PaginationOutputDto<Items = any> = {
+  items: Items[]
+  total: number
+  currentPage: number
+  lastPage: number
+  perPage: number
+}
+
+export class PaginationOutputMapper {
+  static toOutput(result: SearchResult): Omit<PaginationOutputDto, 'items'> {
+    return {
+      total: result.total,
+      currentPage: result.currentPage,
+      lastPage: result.lastPage,
+      perPage: result.perPage
+    }
+  }
+}
