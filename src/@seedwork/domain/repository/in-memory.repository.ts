4e2051212e17f928a269dc9a78a3b6c134eaf76e
André <silva.ala82@gmail.com>
2024-10-16 03:01:10 +0000

    feat: implements im memory repository class with searchable fields

--- src/@seedwork/domain/repository/in-memory.repository.ts
+++ src/@seedwork/domain/repository/in-memory.repository.ts
@@ -1,9 +1,9 @@
 import Entity from "../entity/entity";
 import NotFoundError from "../errors/not-found.error";
 import UniqueEntityId from "../value-objects/unique-entity-id.vo";
-import { RepositoryInterface } from "./repository-contratcs";
+import { RepositoryInterface, SearchableRepositoryInterface, SearchParams, SearchResult, SortDirection } from "./repository-contratcs";
 
-export default abstract class InMemoryRepository<E extends Entity> implements RepositoryInterface<E> {
+export abstract class InMemoryRepository<E extends Entity> implements RepositoryInterface<E> {
 
   items: E[] = []
 
@@ -35,5 +35,53 @@ export default abstract class InMemoryRepository<E extends Entity> implements Re
       throw new NotFoundError(`Entity Not Found using ID ${id}`)
     }
     return item
+  }
+}
+
+export abstract class InMemorySearchableRepository<E extends Entity>
+  extends InMemoryRepository<E>
+  implements SearchableRepositoryInterface<E> {
+
+  sortableFields: string[] = [];
+
+  async search(props: SearchParams): Promise<SearchResult<E>> {
+    const itemsFiltered = await this.applyFilter(this.items, props.filter)
+    const itemsSorted = await this.applySort(itemsFiltered, props.sort, props.sortDir)
+    const itemsPaginated = await this.applyPaginate(itemsSorted, props.page, props.perPage)
+
+    return new SearchResult({
+      items: itemsPaginated,
+      total: itemsFiltered.length,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: props.sort,
+      sortDir: props.sortDir,
+      filter: props.filter
+    })
+  }
+
+  protected abstract applyFilter(items: E[], filter: string | null): Promise<E[]>
+
+  protected async applySort(items: E[], sort: string | null, sortDir: SortDirection | null): Promise<E[]> {
+
+    if (!sort || !this.sortableFields.includes(sort)) {
+      return items
+    }
+
+    return [...items].sort((a, b) => {
+      if (a.props[sort] < b.props[sort]) {
+        return sortDir === 'asc' ? -1 : 1
+      }
+      if (a.props[sort] > b.props[sort]) {
+        return sortDir === 'asc' ? 1 : -1
+      }
+      return 0
+    })
+  }
+
+  protected async applyPaginate(items: E[], page: SearchParams['page'], perPage: SearchParams['perPage']): Promise<E[]> {
+    const start = (page - 1) * perPage  // 0 * 15 = 0
+    const limit = start + perPage // 0 + 15 = 15
+    return items.slice(start, limit)
   }
 }
---
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -0,0 +1,9 @@
+import { Category } from "../../../category/domain/entities/category";
+import { InMemorySearchableRepository } from "../../../@seedwork/domain/repository/in-memory.repository";
+import CategoryRepository from "../../../category/domain/repository/category.repository";
+
+export default class CategoryInMemoryRepository
+  extends InMemorySearchableRepository<Category>
+  implements CategoryRepository {
+
+}
