31a9bfca438141c9f9c3f03a9c8ba9b72adf5580
André <silva.ala82@gmail.com>
2024-10-16 19:03:53 +0000

    feat: implements category output

--- src/category/application/use-cases/create-category.use-case.ts
+++ src/category/application/use-cases/create-category.use-case.ts
@@ -1,6 +1,6 @@
 import CategoryRepository from "category/domain/repository/category.repository"
 import { Category } from "../../../category/domain/entities/category"
-import { CategoryOutput } from "../dto/category-output.dto"
+import { CategoryOutput, CategoryOutputMapper } from "../dto/category-output"
 import UseCase from "../../../@seedwork/application/use-case"
 
 export default class CreateCategoryUseCase implements UseCase<Input, Output> {
@@ -10,13 +10,7 @@ export default class CreateCategoryUseCase implements UseCase<Input, Output> {
   async execute(input: Input): Promise<Output> {
     const entity = new Category(input)
     await this.categoryRepo.insert(entity)
-    return {
-      id: entity.id,
-      name: entity.name,
-      description: entity.description,
-      isActive: entity.isActive,
-      createdAt: entity.createdAt
-    }
+    return CategoryOutputMapper.toOutput(entity)
   }
 }
 
