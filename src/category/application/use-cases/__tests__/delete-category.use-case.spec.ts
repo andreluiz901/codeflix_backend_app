847ebf23586ef20a2b465c13e7d4f90f6facff64
André <silva.ala82@gmail.com>
2024-10-17 00:14:30 +0000

    test: ensures that should be able to delete a category

---
+++ src/category/application/use-cases/__tests__/delete-category.use-case.spec.ts
@@ -0,0 +1,27 @@
+import CategoryInMemoryRepository from "../../../infra/repository/category-in-memory.repository"
+import DeleteCategoryUseCase from "../delete-category.use-case"
+import NotFoundError from "../../../../@seedwork/domain/errors/not-found.error"
+import { Category } from "../../../domain/entities/category"
+
+describe('DeleteCategoryUseCase Unit Test', () => {
+  let useCase: DeleteCategoryUseCase
+  let repository: CategoryInMemoryRepository
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository()
+    useCase = new DeleteCategoryUseCase(repository)
+  })
+
+  it('should throws error when entity not found', async () => {
+    expect(() => useCase.execute({ id: 'fake id' })).rejects.toThrow(
+      new NotFoundError(`Entity Not Found using ID fake id`)
+    )
+  })
+
+  it('should delete a category', async () => {
+    const items = [new Category({ name: 'test' })]
+    repository.items = items
+    await useCase.execute({ id: items[0].id })
+    expect(repository.items).toHaveLength(0)
+  })
+})
