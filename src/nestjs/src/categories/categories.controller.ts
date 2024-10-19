b9ac9cd7f505968a156665b3d513aaf07cff4c2c
André <silva.ala82@gmail.com>
2024-10-19 19:07:48 +0000

    chore: add Biome to lint and format project inside of conteiner

--- src/nestjs/src/categories/categories.controller.ts
+++ src/nestjs/src/categories/categories.controller.ts
@@ -1,40 +1,55 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CategoriesService } from './categories.service';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
-import { CreateCategoryUseCase, ListCategoriesUseCase } from 'codeflix-backend-app/category/application';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Patch,
+	Post,
+} from "@nestjs/common";
+import {
+	CreateCategoryUseCase,
+	ListCategoriesUseCase,
+} from "codeflix-backend-app/category/application";
+import { CategoriesService } from "./categories.service";
+import { CreateCategoryDto } from "./dto/create-category.dto";
+import { UpdateCategoryDto } from "./dto/update-category.dto";
 
-@Controller('categories')
+@Controller("categories")
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService,
-    private readonly createUseCase: CreateCategoryUseCase.UseCase,
-    private readonly listUseCase: ListCategoriesUseCase.UseCase,
-  ) { }
+	constructor(
+		private readonly categoriesService: CategoriesService,
+		private readonly createUseCase: CreateCategoryUseCase.UseCase,
+		private readonly listUseCase: ListCategoriesUseCase.UseCase,
+	) {}
 
-  @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.createUseCase.execute({ name: 'teste' })
-    //return this.categoriesService.create(createCategoryDto);
-  }
+	@Post()
+	create(@Body() createCategoryDto: CreateCategoryDto) {
+		return this.createUseCase.execute({ name: "teste" });
+		//return this.categoriesService.create(createCategoryDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.listUseCase.execute({})
-    return this.categoriesService.findAll();
-  }
+	@Get()
+	findAll() {
+		return this.listUseCase.execute({});
+		// return this.categoriesService.findAll();
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoriesService.findOne(+id);
-  }
+	@Get(":id")
+	findOne(@Param("id") id: string) {
+		return this.categoriesService.findOne(+id);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoriesService.update(+id, updateCategoryDto);
-  }
+	@Patch(":id")
+	update(
+		@Param("id") id: string,
+		@Body() updateCategoryDto: UpdateCategoryDto,
+	) {
+		return this.categoriesService.update(+id, updateCategoryDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoriesService.remove(+id);
-  }
+	@Delete(":id")
+	remove(@Param("id") id: string) {
+		return this.categoriesService.remove(+id);
+	}
 }
